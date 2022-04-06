a7b8f2badc166cd539c2d5d239fd79075118c6b1
Branus <72809118+preboy-lk@users.noreply.github.com>
2022-04-07 00:14:44 +0700

    logo

---
+++ TextInput.py
@@ -0,0 +1,61 @@
+import pygame as pg
+
+
+pg.init()
+#screen = pg.display.set_mode((640, 480))
+COLOR_INACTIVE = pg.Color('White')
+COLOR_ACTIVE = pg.Color('Black')
+FONT = pg.font.SysFont(None, 20)
+
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pg.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    pass
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+    def ShowValue(self):
+        return self.text
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pg.draw.rect(screen, self.color, self.rect, 2)
+
+
+def CreateInputBox():
+    input_box1 = InputBox(1480, 550, 10, 21)
+    input_box2 = InputBox(1500, 570, 10, 21)
+    input_box3 = InputBox(1480, 650, 10, 21)
+    input_box4 = InputBox(1500, 670, 10, 21)
+    input_boxes = [input_box1, input_box2, input_box3, input_box4]
+    pg.key.set_repeat(200, 25)
+    return input_boxes
